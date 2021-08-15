f4df6a55b481504398bf121f4997bfc6b7a54195
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-08-15 15:29:32 +0900

    Implementation of main function #3

--- GraphColoring/GraphColoring/main.cpp
+++ GraphColoring/GraphColoring/main.cpp
@@ -59,27 +59,30 @@ bool make_graph() {
 
 
 int selecting(int t_num){
-	tcb[t_num]->set_t_flag(SELETING);
+	tcb[t_num]->set_t_flag(T_FLAG::SELECTING);
 	
-	int tmp_flag;
-	int index=0;
-	bool fin=true;
-	for(index =0; index<tcb[t_num]->task.size(); index++){
-		tmp_flag = tcb[t_num]->task[index]->n_flag; 
-		if(tmp_flag==CAN_SELECT){
-			fin=false;
+	int tmp_flag, index = 0;
+	bool fin = true;
+
+	for (index = 0; index < tcb[t_num]->task.size(); index++) {
+		tmp_flag = tcb[t_num]->task[index]->n_flag;
+		if (tmp_flag == N_FLAG::CAN_SELECT) {
+			fin = false;
 			break;
 		}
-		if(tmp_flag==WAIT)
-			fin=false;
+		else if (tmp_flag == N_FLAG::WAIT) {
+			fin = false;
+		}
 		
-		if(index==(tcb[t_num]->task.size()-1)){
-			if(fin){
+		if (index == (tcb[t_num]->task.size() - 1)) {
+			if (fin) {
 				// 쓰레드 종료
-				tcb[t_num]->set_t_flag(DONE);
+				tcb[t_num]->set_t_flag(T_FLAG::DONE);
 				index = -1;
 				break;
-			}else{	// 아직 선택할 노드가 남아있으면 처음부터 다시 탐색
+			}
+			// 아직 선택할 노드가 남아있으면 처음부터 다시 탐색
+			else {
 				index = -1;
 			}
 		}
@@ -88,75 +91,81 @@ int selecting(int t_num){
 }
 
 int waitting(int t_num, int index){
-	
-	
-	tcb[t_num]->task[index]->set_n_flag(SELECTED);
-	tcb[t_num]->set_t_flag(WAIT);
+	tcb[t_num]->task[index]->set_n_flag(N_FLAG::SELECTED);
+	tcb[t_num]->set_t_flag(T_FLAG::WAIT);
+
 	int tmp_flag;
 	// wait
-	int next = COLORING;
-	for(int i=0; i<tcb[t_num]->task[index]->adjacent.size();i++){
-		Node* adj = tcb[t_num]->task[index]->adjacent[i];
+	int next = T_FLAG::COLORING;
+	Node* task = tcb[t_num]->task[index];
+	for (int i = 0; i < task->adjacent.size(); i++) {
+		Node* adj = task->adjacent[i];
+		// adj->t_num 을
+		// adj->index % tcb.size() 로 변경해도 된다.
 		tmp_flag = tcb[adj->t_num]->t_flag;
 			
 		// 여기부분이 쪼금 맘에 안들긴한다.... 아이디어있으면 알려주셈
 		// 해당 쓰레드만 다시 검사할수있는 방법이 있으면 좋을거같긴함,,,
-		if(tmp_flag == SELETING)
-			next = WAIT;
-			
-		if(tmp_flag == COLORING){
-			if(adj->n_flag == SELECTED){
-				next = SELECTING;
+		if (tmp_flag == T_FLAG::SELECTING) {
+			next = T_FLAG::WAIT;
+		}
+		else if (tmp_flag == T_FLAG::COLORING) {
+			if (adj->n_flag == N_FLAG::SELECTED) {
+				next = T_FLAG::SELECTING;
 				break;
 			}
-			if(adj->n_flag == CAN_SELECT){
-				adj->set_n_flag(WAIT);
+			else if (adj->n_flag == N_FLAG::CAN_SELECT) {
+				adj->set_n_flag(N_FLAG::WAIT);
 				// 문제가능성있음
 				// WAIT으로 바꾸기 이전에 해당 쓰레드가 coloring을 마치고 해당 노드를 선택
 				// 혹은 현재쓰레드가 경쟁에서 밀릴 가능성,,,
 			}
 		}
-		if(i==tcb[t_num]->task[index]->adjacent.size()-1){
-			if(next==WAIT){
-				next==COLORING;
-				i=-1;
+
+		if (i == task->adjacent.size() - 1) {
+			if (next == T_FLAG::WAIT) {
+				next = T_FLAG::COLORING;
+				i = -1;
 			}
-			else
+			else {
 				break;
+			}
 		}
 	}
 	return next;
 }
 
-void coloring() {
+void coloring(int t_num, int index) {
+	tcb[t_num]->set_t_flag(T_FLAG::COLORING);
+	
+	int tmp_color = 0;
+	Node* task = tcb[t_num]->task[index];
 
-	
-	
-	tcb[t_num]->set_t_flag(COLORING);
-	int tmp_color=0;
-	
 	// 선택한 node의 색상칠하는 과정 - n_color에서 가장먼저 true 인 지점의 index를 color값으로 지정
-	for(;tmp_color<tcb[t_num].task[index]->n_color.size(); tmp_color++){
-		if(tcb[t_num].task[index]->n_color[tmp_color])
-			tcb[t_num].task[index]->set_n_flag(COLORED);
+	for (; tmp_color < task->n_color.size(); tmp_color++) {
+		if (task->n_color[tmp_color])
+			task->set_n_flag(N_FLAG::COLORED);
 			break;
 	}
-	tcb[t_num].task[index]->color = tmp_color;
+	task->color = tmp_color;
 	
 	// 선택한 node의 adjacent들의 n_color 수정 및 adjacent에서 node 제거.
-	for(int i=0; i<tcb[t_num].task[index]->adjacent.size(); i++){
-		tcb[t_num].task[index]->adjacent[i]->n_color[tmp_color] = false;		// n_color 변경
-		int j=0;
-		
+	for (int i = 0; i < task->adjacent.size(); i++) {
+		Node* adj = task->adjacent[i];
+		// n_color 변경
+		adj->n_color[tmp_color] = false;
+
+		int j = 0;
 		// adjacent의 adjacent 에서 선택된 node 제거하는 과정..
-		while(1){
-			if(tcb[t_num].task[index]->adjacent[i]->adjacent[j]->index == tcb[t_num].task[index]->adjacent[i]->index){
+		while (1) {
+			// 이 부분 살짝 이상한데?
+			if (adj->adjacent[j]->index == adj->index) {
 				// 해당 node가 선택된 node 때문에 WAIT이 걸렸을수 있기 때문에 WAIT인경우 풀어준다.
-				if(tcb[t_num].task[index]->adjacent[i]->adjacent[j]->n_flag==WAIT){
-					tcb[t_num].task[index]->adjacent[i]->adjacent[j]->set_n_flag(CAN_SELECT);
+				if (adj->adjacent[j]->n_flag == N_FLAG::WAIT) {
+					adj->adjacent[j]->set_n_flag(N_FLAG::CAN_SELECT);
 				}
 					
-				tcb[t_num].task[index]->adjacent[i]->adjacent.erase(tcb[t_num].task[index]->adjacent[i]->adjacent.begin()+j);
+				adj->adjacent.erase(adj->adjacent.begin() + j);
 				break;
 			}
 			j++;
@@ -165,30 +174,21 @@ void coloring() {
 }
 
 void t_work(int t_num) {
-	
-	// init
-	// vector<*Node>* t_task = tcb[t_num]->task; 이렇게 바꿔도 되려나??
+	// thread 별로 가지고 있는 task를 degree 기준으로 내림차순 정렬함.
 	sort(tcb[t_num]->task.begin(), tcb[t_num]->task.end(), Node::compare);
 	
-	
-	// 각 함수는 t_flag가 변경되는 기준으로 만들어짐
-	// 각 함수에서 n_flag가 변경될때 해당부분에서 문제가 생길가능성이없도록 만들면될듯,,,
-	// 선택된 node가 붙어있는 경우 따로 처리를 안함 추가해야함 원래는 coloring에 넣으려구 했는데, 지금보니까 그냥 wait에서 처리해도 될랑가싶기두하고,,
 	while(1){
-		// select
-		int node_index;	// select하고 선택한 node의 index값
-		node_index = selecting(int t_num);
-		if(node_index == -1;){
-			// 쓰레드 종료
-			
+		// select하고 선택한 node의 index값
+		int node_index = selecting(t_num);
+		if (node_index == -1) {
+			// thread exit
 			break;
 		}
-		if(waitting(t_num, node_index)==SELECTING)
+		if (waitting(t_num, node_index) == T_FLAG::SELECTING)
 			continue;
-		//if(next==COLORING)
+
 		coloring(t_num, node_index);
 	}
-
 }
 
 int main(void) {
