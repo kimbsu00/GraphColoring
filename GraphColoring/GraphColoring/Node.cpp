a79b6ce783b7ff0eea9e2ca4a12223aeea9f9328
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-08-17 16:43:50 +0900

    check_adjacent and coloring functions have been added.

--- GraphColoring/GraphColoring/Node.cpp
+++ GraphColoring/GraphColoring/Node.cpp
@@ -20,6 +20,11 @@ Node::~Node()
 {
 }
 
+bool Node::compare(const Node* a, const Node* b)
+{
+	return a->degree > b->degree;
+}
+
 bool Node::check()
 {
 	for (int i = 0; i < adjacent.size(); i++) {
@@ -44,7 +49,49 @@ void Node::update_n_color(int already_used_color)
 	this->n_color_mutex.unlock();
 }
 
-bool Node::compare(const Node* a, const Node* b)
+bool Node::check_adjacent()
 {
-	return a->degree > b->degree;
+	bool ret = true;
+	restore_index.clear();
+
+	for (int i = 0; i < adjacent.size(); i++) {
+		if (adjacent[i]->n_flag == N_FLAG::SELECTED) {
+			for (int j = 0; j < restore_index.size(); j++) {
+				adjacent[i]->set_n_flag(N_FLAG::CAN_SELECT);
+			}
+			ret = false;
+			break;
+		}
+		
+		adjacent[i]->n_flag_mutex.lock();
+		if (adjacent[i]->n_flag == N_FLAG::CAN_SELECT) {
+			adjacent[i]->n_flag = N_FLAG::N_WAIT;
+			restore_index.push_back(i);
+		}
+		adjacent[i]->n_flag_mutex.unlock();
+	}
+
+	return ret;
+}
+
+void Node::coloring()
+{
+	int color = 0;
+	n_color_mutex.lock();
+	for (; color < n_color.size(); color++) {
+		if (n_color[color]) {
+			this->color = color;
+			break;
+		}
+	}
+	n_color_mutex.unlock();
+	set_n_flag(N_FLAG::COLORED);
+
+	for (int i = 0; i < adjacent.size(); i++) {
+		adjacent[i]->update_n_color(color);
+	}
+
+	for (int i = 0; i < restore_index.size(); i++) {
+		adjacent[restore_index[i]]->set_n_flag(N_FLAG::CAN_SELECT);
+	}
 }
--- GraphColoring/GraphColoring/Node.h
+++ GraphColoring/GraphColoring/Node.h
@@ -7,7 +7,7 @@ using namespace std;
 enum N_FLAG {
 	CAN_SELECT,
 	SELECTED,
-	WAIT,
+	N_WAIT,
 	COLORED
 };
 
@@ -20,8 +20,6 @@ public:
 	int degree;
 	// 직접 연결되어 있는 Node들
 	vector<Node*> adjacent;
-	// adjacnet의 copy
-	vector<Node*> c_adjacent;
 	// Node의 현재 상태를 의미함
 	int n_flag;
 	// n_flag에 대한 mutex
@@ -34,17 +32,20 @@ public:
 	mutex n_color_mutex;
 	// 해당 노드를 작업할 쓰레드 번호
 	int t_num;
- 	
+	// n_flag의 값을 N_FLAG::WAIT으로 변경했던 adjacent의 index를 의미함
+	vector<int> restore_index;
 	
 	Node();
 	Node(int index);
 	Node(int index, int num_of_node);
 	~Node();
 
+	static bool compare(const Node* a, const Node* b);
+
 	bool check();
 	void set_n_flag(int n_flag);
 	void update_n_color(int already_used_color);
-
-	static bool compare(const Node* a, const Node* b);
+	bool check_adjacent();
+	void coloring();
 };
 
