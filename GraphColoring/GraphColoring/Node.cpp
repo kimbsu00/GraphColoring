844347e5e62384a0dca672eb96eccdaabde023f7
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-09-03 21:04:40 +0900

    source code has been modified.

--- GraphColoring/GraphColoring/Node.cpp
+++ GraphColoring/GraphColoring/Node.cpp
@@ -49,7 +49,7 @@ bool Node::is_priority()
 	return ret;
 }
 
-void Node::coloring()
+long long Node::coloring()
 {
 	int color = 0;
 	for (; color < n_color.size(); color++) {
@@ -76,11 +76,11 @@ void Node::coloring()
 	* 인접한 두 노드가 서로 같은 색이 될 수 있다.
 	*/ 
 	omp_set_lock(&degree_lock);
-	//this->degree_mutex.lock();
 	this->degree = -1;
-	//this->degree_mutex.unlock();
 	omp_unset_lock(&degree_lock);
 
 	this->n_flag = N_FLAG::COLORED;
+
+	return adjacent.size();
 }
 
