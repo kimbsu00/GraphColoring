1654c270a1077dad665228d206da1b8eb7c74793
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-10-04 22:14:02 +0900

    priority has been added.

--- GraphColoring/GraphColoring/Node.cpp
+++ GraphColoring/GraphColoring/Node.cpp
@@ -1,26 +1,30 @@
 #include "Node.h"
 
 Node::Node()
-	: index(-1), degree(0), n_flag(UNCOLORED), color(-1)
+	: index(-1), degree(0), n_flag(UNCOLORED), color(-1), priority(-1)
 {
+	omp_init_lock(&priority_lock);
 	omp_init_lock(&n_color_lock);
 }
 
 Node::Node(int index)
-	: index(index), degree(0), n_flag(UNCOLORED), color(-1)
+	: index(index), degree(0), n_flag(UNCOLORED), color(-1), priority(-1)
 {
+	omp_init_lock(&priority_lock);
 	omp_init_lock(&n_color_lock);
 }
 
 Node::Node(int index, int num_of_node)
-	: index(index), degree(0), n_flag(UNCOLORED), color(-1)
+	: index(index), degree(0), n_flag(UNCOLORED), color(-1), priority(-1)
 {
+	omp_init_lock(&priority_lock);
 	omp_init_lock(&n_color_lock);
 	n_color.resize(num_of_node, true);
 }
 
 Node::~Node()
 {
+	omp_destroy_lock(&priority_lock);
 	omp_destroy_lock(&n_color_lock);
 }
 
@@ -29,6 +33,13 @@ bool Node::compare(const Node* a, const Node* b)
 	if (a->degree == b->degree)
 		return a->index < b->index;
 	return a->degree > b->degree;
+}
+
+bool Node::compare_by_priority(const Node* a, const Node* b)
+{
+	if (a->priority == b->priority)
+		return a->index < b->index;
+	return a->priority < b->priority;
 }
 
 bool Node::is_priority()
@@ -43,6 +54,16 @@ bool Node::is_priority()
 	}
 
 	return ret;
+}
+
+void Node::calculate_priority()
+{
+	int tmp = 0;
+
+	for (int i = 0; i < adjacent.size(); i++) {
+		if (!compare(this, adjacent[i]))			tmp += 1;
+	}
+	this->priority = tmp;
 }
 
 long long Node::coloring()
@@ -61,11 +82,16 @@ long long Node::coloring()
 		omp_unset_lock(&(adjacent[i]->n_color_lock));
 	}
 
-	/*
-	* this->degree를 adjacent[i]->degree보다 먼저 변경할 경우,
-	* 인접한 두 노드가 서로 같은 색이 될 수 있다.
-	*/ 
+	for (int i = 0; i < adjacent.size(); i++) {
+		if (adjacent[i]->n_flag == N_FLAG::COLORED)			continue;
+
+		omp_set_lock(&(adjacent[i]->priority_lock));
+		adjacent[i]->priority -= 1;
+		omp_unset_lock(&(adjacent[i]->priority_lock));
+	}
+
 	this->degree = -1;
+	this->priority = -1;
 
 	this->n_flag = N_FLAG::COLORED;
 
--- GraphColoring/GraphColoring/Node.h
+++ GraphColoring/GraphColoring/Node.h
@@ -17,6 +17,11 @@ public:
 	int index;
 	// 직접 연결되어 있는 Node의 수
 	int degree;
+	// 직접 연결되어 있는 Node들과 비교했을 때의 상대적 우선순위
+	// 0이면 색칠할 수 있는 상태임을 의미한다.
+	int priority;
+	// priority에 대한 lock
+	omp_lock_t priority_lock;
 	// 직접 연결되어 있는 Node들
 	vector<Node*> adjacent;
 	// Node의 현재 상태를 의미함
@@ -33,7 +38,15 @@ public:
 	Node(int index, int num_of_node);
 	~Node();
 
+	/*
+	* degree를 기준으로 Node를 비교하는 함수
+	*/
 	static bool compare(const Node* a, const Node* b);
+
+	/*
+	* priority를 기준으로 Node를 비교하는 함수
+	*/
+	static bool compare_by_priority(const Node* a, const Node* b);
 
 	/*
 	* 인접한 노드들과 비교했을 때,
@@ -41,6 +54,14 @@ public:
 	* 그렇지 않으면 return false
 	*/
 	bool is_priority();
+
+	/*
+	* 인접한 노드들과 비교했을 때,
+	* priority(상대적 우선순위)를 계산한다.
+	* priority == 0 -> 색칠 가능한 경우를 의미
+	* priority > 0  -> 먼저 색칠해야 하는 노드가 priority개 만큼 존재함을 의미
+	*/
+	void calculate_priority();
 
 	/*
 	* Node를 색칠하고, 색칠한 결과를 인접한 Node에 반영한다.
